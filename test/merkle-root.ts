f8f3a78104b9d4491158f04754b13ce8e1f26f38
Abiodun <aabiodunawoyemi@gmail.com>
2022-08-25 12:13:00 +0100

    script for merkle tree

--- test/merkle-root.ts
+++ test/merkle-root.ts
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 const keccak256 = require("keccak256");
 const { MerkleTree } = require("merkletreejs");
 
-function encodeLeaf(address: any, spots: any) {
+function encodeLeaf(address: any, spots: number) {
   // Same as `abi.encodePacked` in Solidity
   return ethers.utils.defaultAbiCoder.encode(
     ["address", "uint64"],
